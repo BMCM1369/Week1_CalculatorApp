47955b3f035ce739a8d6e2e67e57f00eeefcce06
benmc <McManus-B8@ulster.ac.uk>
2025-09-28 13:41:15 +0100

    Coded...

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.ComponentModel;
+
+Console.WriteLine("Please enter the first number");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the second number");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = FirstNumber + SecondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", FirstNumber, SecondNumber, result);
